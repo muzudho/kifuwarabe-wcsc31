e95d31ab05ce097d4a41a2b404245c2e840e96cc
むずでょ <muzudho1@gmail.com>
2021-06-25 23:22:03 +0900

    Dump

--- take16/brain_print.go
+++ take16/brain_print.go
@@ -10,35 +10,22 @@ func (pBrain *Brain) Dump() string {
 	// bytes.Bufferは、速くはないけど使いやすいぜ（＾～＾）
 	var buffer bytes.Buffer
 
+	// Each position
 	for b := PosLayerT(0); b < 2; b += 1 {
 		pPos := pBrain.PPosSys.PPosition[b]
 		buffer.WriteString(fmt.Sprintf("Position[%d]:\n", b))
-		for i := 0; i < POS_LAYER_SIZE; i += 1 {
-			buffer.WriteString(pBrain.PPosSys.PPosition[i].SprintBoard())
-			//buffer.WriteString(fmt.Sprintf("%d,", pBrain.PPosSys.PPosition[i].Board))
-		}
+
+		// Board, Hands
+		buffer.WriteString(pPos.SprintBoard())
 		buffer.WriteString("\n")
 
 		// PieceLocation
 		pPos.SprintLocation()
-		// buffer.WriteString(fmt.Sprintf("KingLocations[%d]:%d,%d\n", b, pPos.PieceLocations[PCLOC_K1], pPos.PieceLocations[PCLOC_K2]))
-		// buffer.WriteString(fmt.Sprintf("RookLocations[%d]:%d,%d\n", b, pPos.PieceLocations[PCLOC_R1], pPos.PieceLocations[PCLOC_R2]))
-		// buffer.WriteString(fmt.Sprintf("BishopLocations[%d]:%d,%d\n", b, pPos.PieceLocations[PCLOC_B1], pPos.PieceLocations[PCLOC_B2]))
-		// buffer.WriteString(fmt.Sprintf("LanceLocations[%d]:%d,%d,%d,%d\n", b, pPos.PieceLocations[PCLOC_L1], pPos.PieceLocations[PCLOC_L2], pPos.PieceLocations[PCLOC_L3], pPos.PieceLocations[PCLOC_L4]))
 	}
 
 	// 利きボード全部
 	for c := ControlLayerT(0); c < CONTROL_LAYER_ALL_SIZE; c += 1 {
 		buffer.WriteString(pBrain.PCtrlBrdSys.SprintControl(c))
-	}
-
-	for b := PosLayerT(0); b < 2; b += 1 {
-		buffer.WriteString(fmt.Sprintf("Position[%d]:", b))
-		buffer.WriteString("Hands:")
-		for i := HAND_IDX_START; i < HAND_IDX_END; i += 1 {
-			buffer.WriteString(fmt.Sprintf("%d,", pBrain.PPosSys.PPosition[b].Hands1[i]))
-		}
-		buffer.WriteString("\n")
 	}
 
 	buffer.WriteString(fmt.Sprintf("Phase:%d,\n", pBrain.PPosSys.GetPhase()))
