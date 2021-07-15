2c6872b7a53785fa565d9282623a21252a6671c7
むずでょ <muzudho1@gmail.com>
2021-07-15 17:56:08 +0900

    探索部変更（＾～＾）

--- take16/search.go
+++ take16/search.go
@@ -11,7 +11,7 @@ import (
 // 1 の 2手読みにしておくと、玉を取りに行くぜ（＾～＾）
 // 2 の 3手読みだと駒を取らない（＾～＾）駒のただ捨てをする（＾～＾）駒をとりかえさない（＾～＾）
 // 3 の 4手読みは、まだ遅い（＾～＾）
-var depthEnd int = 1
+var maxDepth int = 1
 
 type SearchType uint8
 
@@ -23,8 +23,8 @@ const (
 	SEARCH_CAPTURE = SearchType(1)
 )
 
-const RESIGN_VALUE = p.Value(-2_147_483_647)     // Value(-32767)
-const ANTI_RESIGN_VALUE = p.Value(2_147_483_647) // Value(32767)
+// 最大限に使わなくても、十分に大きければ十分だが（＾～＾）
+const VALUE_INFINITE = 2_147_483_647
 
 var nodesNum int
 
@@ -42,26 +42,35 @@ const (
 func SearchEntry(pNerve *Nerve) b.Move {
 
 	nodesNum = 0
-	curDepth := 0
+	depth := maxDepth
 	//fmt.Printf("Search: depth=%d/%d nodesNum=%d\n", curDepth, depthEnd, nodesNum)
 
-	nodeValue, bestMove := search(pNerve, curDepth, SEARCH_NONE)
+	var alpha p.Value = -VALUE_INFINITE
+	var beta p.Value = VALUE_INFINITE
+
+	nodeValue, bestMove := search(pNerve, alpha, beta, depth, SEARCH_NONE)
 
 	// 評価値出力（＾～＾）
 	G.Chat.Print("info depth %d nodes %d score cp %d currmove %s pv %s\n",
-		curDepth, nodesNum, nodeValue, p.ToMoveCode(bestMove), p.ToMoveCode(bestMove))
+		depth, nodesNum, nodeValue, p.ToMoveCode(bestMove), p.ToMoveCode(bestMove))
 
 	// ゲーム向けの軽い乱数
 	return bestMove
 }
 
 // search - 探索部
-func search(pNerve *Nerve, curDepth int, search_type SearchType) (p.Value, b.Move) {
+func search(pNerve *Nerve, alpha p.Value, beta p.Value, depth int, search_type SearchType) (p.Value, b.Move) {
 	//fmt.Printf("Search2: depth=%d/%d nodesNum=%d\n", curDepth, depthEnd, nodesNum)
 
 	// TODO 葉ノード
-	if depthEnd <= curDepth {
+	if depth <= 0 {
+		// 葉ノード
+		// 駒割り評価値は、相手の手番のものになっています。
+		materialValue := pNerve.PPosSys.PPosition[POS_LAYER_MAIN].MaterialValue
+		//fmt.Printf("move=%s leafVal=%6d materialVal=%6d(%s) control_val=%6d\n", move.ToCode(), leafVal, materialVal, captured.ToCode(), control_val)
 
+		// 指し手は返さないぜ（＾～＾）
+		return materialValue, p.RESIGN_MOVE
 	}
 
 	// 指し手生成
@@ -72,14 +81,12 @@ func search(pNerve *Nerve, curDepth int, search_type SearchType) (p.Value, b.Mov
 
 	if lenOfMoves == 0 {
 		// ステイルメートされたら負け（＾～＾）
-		return RESIGN_VALUE, p.RESIGN_MOVE
+		return -VALUE_INFINITE, p.RESIGN_MOVE
 	}
 
 	// 同じ価値のベストムーブがいっぱいあるかも（＾～＾）
 	var someBestMoves []b.Move
 
-	// 次の相手の手の評価値（自分は これを最小にしたい）
-	var opponentWorstVal p.Value = ANTI_RESIGN_VALUE
 	// 前回のムーブ
 	var younger_sibling_move = p.RESIGN_MOVE
 	// 探索終了
@@ -115,7 +122,7 @@ func search(pNerve *Nerve, curDepth int, search_type SearchType) (p.Value, b.Mov
 		// 取った駒は棋譜の１手前に記録されています
 		captured := pNerve.PRecord.CapturedList[pNerve.PRecord.OffsetMovesIndex-1]
 
-		var leaf = false
+		var edgeValue p.Value = -VALUE_INFINITE
 
 		if pNerve.IsCheckmate(FlipPhase(pNerve.PPosSys.phase)) {
 			// ここで指した方の玉に王手がかかるようなら、被空き王手（＾～＾）
@@ -124,58 +131,41 @@ func search(pNerve *Nerve, curDepth int, search_type SearchType) (p.Value, b.Mov
 			// 玉を取るのは最善手
 			someBestMoves = nil
 			someBestMoves = append(someBestMoves, move)
-			opponentWorstVal = RESIGN_VALUE
+			edgeValue = VALUE_INFINITE
 			cutting = CuttingKingCapture
 		} else if search_type == SEARCH_CAPTURE && captured == p.PIECE_EMPTY {
 			// 駒の取り合いを探索中に、駒を取らなかったら
-			// ただの葉
-			leaf = true
+			// この手は見なかったことにするぜ（＾～＾）
 		} else {
 			// 駒を取っている場合は、探索を延長します
-			if curDepth < depthEnd { // TODO  || captured != p.PIECE_EMPTY
-				var search_type2 SearchType
-				if captured != p.PIECE_EMPTY {
-					search_type2 = SEARCH_CAPTURE
-				} else {
-					search_type2 = search_type
-				}
-
-				// 再帰
-				opponentVal, _ := search(pNerve, curDepth+1, search_type2)
-				// 再帰直後（＾～＾）
-				// G.Chat.Debug(pNerve.PPosSys.Sprint(POS_LAYER_MAIN))
-
-				if opponentVal < opponentWorstVal {
-					// より低い価値が見つかったら更新
-					someBestMoves = nil
-					someBestMoves = append(someBestMoves, move)
-					opponentWorstVal = opponentVal
-				} else if opponentVal == opponentWorstVal {
-					// 最低値が並んだら配列の要素として追加
-					someBestMoves = append(someBestMoves, move)
-				}
-
+			// TODO  if captured != p.PIECE_EMPTY
+			var search_type2 SearchType
+			if captured != p.PIECE_EMPTY {
+				search_type2 = SEARCH_CAPTURE
 			} else {
-				// 葉ノード
-				leaf = true
+				search_type2 = search_type
+			}
+
+			// 再帰
+			nodeValue, _ := search(pNerve, -beta, -alpha, depth-1, search_type2)
+			edgeValue = -nodeValue
+			// 再帰直後（＾～＾）
+			// G.Chat.Debug(pNerve.PPosSys.Sprint(POS_LAYER_MAIN))
+
+			if alpha < edgeValue {
+				// アルファー・アップデート
+				someBestMoves = nil
+				someBestMoves = append(someBestMoves, move)
+				alpha = edgeValue
+			} else if alpha <= edgeValue {
+				// アルファー・アップデートしないが、同着なら配列の要素として追加
+				someBestMoves = append(someBestMoves, move)
 			}
 		}
 
-		if leaf {
-			// 葉ノード
-			// 駒割り評価値は、相手の手番のものになっています。
-			materialVal := pNerve.PPosSys.PPosition[POS_LAYER_MAIN].MaterialValue
-			//fmt.Printf("move=%s leafVal=%6d materialVal=%6d(%s) control_val=%6d\n", move.ToCode(), leafVal, materialVal, captured.ToCode(), control_val)
-
-			if materialVal < opponentWorstVal {
-				// より低い価値が見つかったら更新
-				someBestMoves = nil
-				someBestMoves = append(someBestMoves, move)
-				opponentWorstVal = materialVal
-			} else if materialVal == opponentWorstVal {
-				// 最低値が並んだら配列の要素として追加
-				someBestMoves = append(someBestMoves, move)
-			}
+		// ベーター・カット
+		if beta < alpha {
+			return -VALUE_INFINITE, p.RESIGN_MOVE
 		}
 
 		pNerve.UndoMove(pNerve.PPosSys.PPosition[POS_LAYER_MAIN])
@@ -218,12 +208,11 @@ func search(pNerve *Nerve, curDepth int, search_type SearchType) (p.Value, b.Mov
 	//fmt.Printf("%d/%d bestmoveListLen=%d\n", curDepth, depthEnd, bestmoveListLen)
 	if bestmoveListLen < 1 {
 		// 指せる手なし
-		return RESIGN_VALUE, p.RESIGN_MOVE
+		return -VALUE_INFINITE, p.RESIGN_MOVE
 	}
 	bestMove = someBestMoves[rand.Intn(bestmoveListLen)]
 	// 評価値出力（＾～＾）
 	// G.Chat.Print("info depth 0 nodes %d score cp %d currmove %s pv %s\n", nodesNum, bestVal, bestMove.ToCode(), bestMove.ToCode())
 
-	// 相手の評価値の逆が、自分の評価値
-	return -opponentWorstVal, bestMove
+	return alpha, bestMove
 }
