2b08f22dd9c9b3d2374336e48c0a0dcfcca29dc0
むずでょ <muzudho1@gmail.com>
2022-04-09 02:00:54 +0900

    models_hand

--- take12/position_system.go
+++ take12/position_system.go
@@ -243,30 +243,6 @@ func PieceFromPhPt(phase Phase, pieceType PieceType) l09.Piece {
 	}
 }
 
-const (
-	// 持ち駒を打つ 0～15 (Index)
-	HAND_K1        = 0
-	HAND_R1        = 1 // 先手飛打
-	HAND_B1        = 2
-	HAND_G1        = 3
-	HAND_S1        = 4
-	HAND_N1        = 5
-	HAND_L1        = 6
-	HAND_P1        = 7
-	HAND_K2        = 8
-	HAND_R2        = 9
-	HAND_B2        = 10
-	HAND_G2        = 11
-	HAND_S2        = 12
-	HAND_N2        = 13
-	HAND_L2        = 14
-	HAND_P2        = 15
-	HAND_SIZE      = 16
-	HAND_TYPE_SIZE = 8
-	HAND_IDX_START = HAND_K1
-	HAND_IDX_END   = HAND_SIZE // この数を含まない
-)
-
 var HandPieceMap1 = [HAND_SIZE]l09.Piece{
 	PIECE_K1, PIECE_R1, PIECE_B1, PIECE_G1, PIECE_S1, PIECE_N1, PIECE_L1, PIECE_P1,
 	PIECE_K2, PIECE_R2, PIECE_B2, PIECE_G2, PIECE_S2, PIECE_N2, PIECE_L2, PIECE_P2}
---
+++ take15/models_hand.go
@@ -0,0 +1,26 @@
+package take15
+
+// Hand piece type (先後付きの持ち駒の種類)
+// 持ち駒を打つときに利用。 0～15
+const (
+	HAND_K1        = 0
+	HAND_R1        = 1 // 先手飛打
+	HAND_B1        = 2
+	HAND_G1        = 3
+	HAND_S1        = 4
+	HAND_N1        = 5
+	HAND_L1        = 6
+	HAND_P1        = 7
+	HAND_K2        = 8
+	HAND_R2        = 9
+	HAND_B2        = 10
+	HAND_G2        = 11
+	HAND_S2        = 12
+	HAND_N2        = 13
+	HAND_L2        = 14
+	HAND_P2        = 15
+	HAND_SIZE      = 16
+	HAND_TYPE_SIZE = 8
+	HAND_IDX_START = HAND_K1
+	HAND_IDX_END   = HAND_SIZE // この数を含まない
+)
