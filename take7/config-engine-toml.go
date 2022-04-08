7a009b9749d3f032db97c9807bbafca7cb146d52
むずでょ <muzudho1@gmail.com>
2022-04-08 23:10:51 +0900

    App

--- take7/config-engine-toml.go
+++ take7/config-engine-toml.go
@@ -27,26 +27,26 @@ func LoadEngineConf(path string) (*EngineConf, error) {
 
 // 外部プロセスとして呼び出される場合、まだロガーの設定が終わってないかも知れません。
 // func debugPrintToml(fileData []byte) {
-// 	// u.App.LogNotEcho.Trace("...Engine content=%s", string(fileData))
+// 	// App.LogNotEcho.Trace("...Engine content=%s", string(fileData))
 
 // 	// Toml解析
 // 	tomlTree, err := toml.Load(string(fileData))
 // 	if err != nil {
 // 		panic(err)
 // 	}
-// 	u.App.LogNotEcho.Trace("...Engine Input:\n")
-// 	u.App.LogNotEcho.Trace("...Engine Engine.Komi=%f\n", tomlTree.Get("Engine.Komi").(float64))
-// 	u.App.LogNotEcho.Trace("...Engine Engine.BoardSize=%d\n", tomlTree.Get("Engine.BoardSize").(int64))
-// 	u.App.LogNotEcho.Trace("...Engine Engine.MaxMoves=%d\n", tomlTree.Get("Engine.MaxMoves").(int64))
-// 	u.App.LogNotEcho.Trace("...Engine Engine.BoardData=%s\n", tomlTree.Get("Engine.BoardData").(string))
+// 	App.LogNotEcho.Trace("...Engine Input:\n")
+// 	App.LogNotEcho.Trace("...Engine Engine.Komi=%f\n", tomlTree.Get("Engine.Komi").(float64))
+// 	App.LogNotEcho.Trace("...Engine Engine.BoardSize=%d\n", tomlTree.Get("Engine.BoardSize").(int64))
+// 	App.LogNotEcho.Trace("...Engine Engine.MaxMoves=%d\n", tomlTree.Get("Engine.MaxMoves").(int64))
+// 	App.LogNotEcho.Trace("...Engine Engine.BoardData=%s\n", tomlTree.Get("Engine.BoardData").(string))
 // }
 // func debugPrintConfig(config e.EngineConf) {
-// 	u.App.LogNotEcho.Trace("...Engine Memory:\n")
-// 	u.App.LogNotEcho.Trace("...Engine Profile.Name=%s\n", config.Profile.Name)
-// 	u.App.LogNotEcho.Trace("...Engine Profile.Pass=%s\n", config.Profile.Pass)
-// 	u.App.LogNotEcho.Trace("...Engine Engine.Komi=%f\n", config.Engine.Komi)
-// 	u.App.LogNotEcho.Trace("...Engine Engine.BoardSize=%d\n", config.Engine.BoardSize)
-// 	u.App.LogNotEcho.Trace("...Engine Engine.MaxMoves=%d\n", config.Engine.MaxMoves)
-// 	u.App.LogNotEcho.Trace("...Engine Engine.MaxMoves=%s\n", config.Engine.BoardData)
-// 	u.App.LogNotEcho.Trace("...Engine Engine.SentinelBoardMax()=%d\n", config.SentinelBoardMax())
+// 	App.LogNotEcho.Trace("...Engine Memory:\n")
+// 	App.LogNotEcho.Trace("...Engine Profile.Name=%s\n", config.Profile.Name)
+// 	App.LogNotEcho.Trace("...Engine Profile.Pass=%s\n", config.Profile.Pass)
+// 	App.LogNotEcho.Trace("...Engine Engine.Komi=%f\n", config.Engine.Komi)
+// 	App.LogNotEcho.Trace("...Engine Engine.BoardSize=%d\n", config.Engine.BoardSize)
+// 	App.LogNotEcho.Trace("...Engine Engine.MaxMoves=%d\n", config.Engine.MaxMoves)
+// 	App.LogNotEcho.Trace("...Engine Engine.MaxMoves=%s\n", config.Engine.BoardData)
+// 	App.LogNotEcho.Trace("...Engine Engine.SentinelBoardMax()=%d\n", config.SentinelBoardMax())
 // }
