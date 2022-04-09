e5f9a2b4591065b145529d6a4d6279b76aaf8514
むずでょ <muzudho1@gmail.com>
2022-04-09 16:32:47 +0900

    WIP piece

--- take9/models_piece.go
+++ take9/models_piece.go
@@ -4,35 +4,35 @@ import "fmt"
 
 // 駒
 const (
-	PIECE_EMPTY = iota
-	PIECE_K1
-	PIECE_R1
-	PIECE_B1
-	PIECE_G1
-	PIECE_S1
-	PIECE_N1
-	PIECE_L1
-	PIECE_P1
-	PIECE_PR1
-	PIECE_PB1
-	PIECE_PS1
-	PIECE_PN1
-	PIECE_PL1
-	PIECE_PP1
-	PIECE_K2
-	PIECE_R2
-	PIECE_B2
-	PIECE_G2
-	PIECE_S2
-	PIECE_N2
-	PIECE_L2
-	PIECE_P2
-	PIECE_PR2
-	PIECE_PB2
-	PIECE_PS2
-	PIECE_PN2
-	PIECE_PL2
-	PIECE_PP2
+	PIECE_EMPTY = iota // 0: 駒なし
+	PIECE_K1           // 1: ▲玉
+	PIECE_R1           // 2: ▲飛
+	PIECE_B1           // 3: ▲角
+	PIECE_G1           // 4: ▲金
+	PIECE_S1           // 5: ▲銀
+	PIECE_N1           // 6: ▲桂
+	PIECE_L1           // 7: ▲香
+	PIECE_P1           // 8: ▲歩
+	PIECE_PR1          // 9: ▲竜
+	PIECE_PB1          //10: ▲馬
+	PIECE_PS1          //11: ▲全
+	PIECE_PN1          //12: ▲圭
+	PIECE_PL1          //13: ▲杏
+	PIECE_PP1          //14: ▲と
+	PIECE_K2           //15: ▽玉
+	PIECE_R2           //16: ▽飛
+	PIECE_B2           //17: ▽角
+	PIECE_G2           //18: ▽金
+	PIECE_S2           //19: ▽銀
+	PIECE_N2           //20: ▽桂
+	PIECE_L2           //21: ▽香
+	PIECE_P2           //22: ▽歩
+	PIECE_PR2          //23: ▽竜
+	PIECE_PB2          //24: ▽馬
+	PIECE_PS2          //25: ▽全
+	PIECE_PN2          //26: ▽圭
+	PIECE_PL2          //27: ▽杏
+	PIECE_PP2          //28: ▽と
 )
 
 // Promote - 成ります
